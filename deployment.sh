edc5a34fcd1ef25da4dace48cfcb9c88e17570bb
IATVirtusa <48546034+IATVirtusa@users.noreply.github.com>
2020-12-24 13:14:56 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -5,7 +5,7 @@ cd /home/metlife-backend/deployments
 
 source metlife-env/bin/activate
 cd metlife_poc 
-echo $$DB_PORT
+echo $DB_PORT
 python manage.py migrate
 
 sudo systemctl stop metlife-api.service
