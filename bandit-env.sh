529b383597df262cabe48cb53940ddbd0953db81
IATVirtusa <48546034+IATVirtusa@users.noreply.github.com>
2020-12-24 10:01:32 +0530

    Create bandit-env.sh

---
+++ bandit-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -d "bandit-env" ] 
+then
+    echo "Python bandit virtual environment exists." 
+else
+    python3 -m venv bandit-env
+fi
+
+source bandit-env/bin/activate
+
+pip3 install bandit
