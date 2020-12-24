75937de57ac712be501a3bd2288eb9195bca934d
IATVirtusa <48546034+IATVirtusa@users.noreply.github.com>
2020-12-24 13:54:26 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -7,7 +7,19 @@ source metlife-env/bin/activate
 cd metlife_poc 
 
 sudo systemctl stop metlife-api.service
+sudo systemctl daemon-reload
 
+sudo systemctl start gunicorn
+
+echo "Gunicorn has started."
+
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been enabled."
+
+sudo systemctl status gunicorn
+
+sudo systemctl restart gunicorn
 
 
 
