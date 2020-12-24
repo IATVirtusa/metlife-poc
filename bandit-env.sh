de17f8dda91341c2b2ef830d8674933b70aabfcf
IATVirtusa <48546034+IATVirtusa@users.noreply.github.com>
2020-12-24 11:38:53 +0530

    Update bandit-env.sh

--- bandit-env.sh
+++ bandit-env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd  /home/metlife-backend/deployments/metlife_poc
+
 if [ -d "bandit-env" ] 
 then
     echo "Python bandit virtual environment exists." 
@@ -11,4 +13,4 @@ source bandit-env/bin/activate
 
 pip3 install bandit
 
-bandit -f html -o /home/lduser/deployments/deployments/metlife_poc/sast_out.html -r /home/lduser/deployments/deployments/metlife_poc/*.py
+bandit -f html -o sast_out.html -r /home/lduser/deployments/deployments/metlife_poc/metlife_poc/*.py
