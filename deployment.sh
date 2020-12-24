89645d0f17d7eb0b3428fd8a8086ad507b17ea39
IATVirtusa <48546034+IATVirtusa@users.noreply.github.com>
2020-12-24 12:41:05 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,1 +1,15 @@
-echo "deployment"
+echo "Backend Deployment"
+
+cd /home/metlife-backend/deployments
+
+source metlife-env/bin/activate
+cd metlife_poc 
+
+python manage.py migrate
+
+systemctl stop metlife-api.service
+
+systemctl start metlife-api.service
+
+
+
